a8afa501d2a2ec7fb514cb8c823e6239b9f673c2
Marius Rønning <143086706+marroe01284@users.noreply.github.com>
2024-01-25 17:09:47 +0100

    added intro screen and initial log in

---
+++ intro/enter.js
@@ -0,0 +1,22 @@
+const ageInput = document.querySelector("#age")
+console.log(ageInput);
+const enterBTN = document.querySelector(".enter-btn")
+
+enterBTN.onclick = function () {
+    const age = ageInput.value;
+
+    const isAgeok = oldEnough(age);
+
+    if (isAgeok) {
+        document.location.href = "../main/home.html";
+    } else {
+        alert("you shall not pass!")
+    }
+}
+
+function oldEnough(ageToCheck) {
+    if (ageToCheck === "OCEAN") {
+        return true;
+    }
+    return false;
+}
